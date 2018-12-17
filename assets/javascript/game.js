81e5d9df57e45bb6321f0105504cf34605e94963
Joe Zufelt <joe.zufelt@gmail.com>
2018-12-17 18:13:26 -0500

    added a bunch of stuff

---
+++ assets/javascript/game.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    var totalQuestions = $(".questions").size();
+    var currentQuestion = 0;
+    var result = 0;
+
+    $questions = $(".questions");
+    $startBtn = $(".startBtn");
+    $endGame = $(".endGame");
+
+    $questions.hide();
+}); //End Ready Function
